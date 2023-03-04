37bca5489d1016432538eb49390c53aba90f8352
AlexisWilke <alexis@m2osw.com>
2023-03-04 09:48:24 -0800

    Fixed copyright dates in murmur3sum tool.

--- tools/murmur3sum.cpp
+++ tools/murmur3sum.cpp
@@ -337,7 +337,7 @@ int main(int argc, char * argv[])
                         << " (Snap! C++ Project) "
                         << MURMUR3_VERSION_STRING
                         << '\n'
-                        << "Copyright (C) 2022 Made to Order Software Corporation\n"
+                        << "Copyright (C) 2022-" << UTC_BUILD_YEAR << " Made to Order Software Corporation\n"
                            "License GPVv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
                            "This is free software: you are free to change and redistribute it.\n"
                            "There is NO WARRANTY, to the extent permitted by law.\n"
